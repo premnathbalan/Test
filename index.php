363e4c9e7bd2161708d739d7f23a5c0b8185cfe0
premnathbalan <premnath.balan@gmail.com>
2017-12-05 11:43:50 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,143 @@
+<?php include  'includes/common.php'; ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<?php include 'meta.txt'; ?>
+<title>About Us | Brahmins Matrimony |  Remarriage Matrimony | Physically Challenged Matrimony | Iyers Matimonys | Iyengar Matrimonys | Iyer Matimony Websites | Iyengar Matrimony Websites | Bride | Matrimony for physically disabled | Remarriages</title>
+<?php include 'includes/css.txt'; ?>
+</head>
+<body id="p2">
+
+<div class="wrap">
+ 
+  <div class="grid4 bg-sub">
+    <div class="grid5">
+      <h3>About us </h3>
+      <h5>For Whom</h5>
+<p>We are extending matrimonial services exclusively for all Brahmins for more than 20 yrs.</p>
+<h5>Origin</h5>
+<p>The organization was started in the year 1990 under the name of Sri Sai horoscope Exchange Centre in Thanjavur.(LIC Colony Medical College Road)as a Free Service Activity with the Divine Blessings of Bhagavan Sri Sathya Sai Baba. Later it was shifted to Chennai at Brindavan Street,West Mambalam and then again got shifted to the present address in Ashok Nagar Since 2007. We do not have any Branches or Tie-up arrangements with any one.</p>
+<h5>Objectives</h5>
+<p>Our Objectives is to enable early settlement of marriages with nominal charges. Our focus is on service orientation with Professional Touch.</p>
+<h5>Website</h5>
+<p>Our website www.ssmatri.com is now extremely popular and seen by Thousands of people daily in India and Abroad. It is absolutely user-friendly and consists of Thousands of profiles with Horoscope and Photos, which can be viewed freely even by non &ndash; members along with contact numbers.  Registration in our website becomes a source of advertisement resulting in enquiries besides being very useful for communication purposes.</p>
+<h5>Categories</h5>
+<p>We classify the data under the following four categories separately to ensure better clarity.</p>
+      <ul>
+        <li>Iyers</li>
+        <li>          Iyengars</li>
+        <li>          Other Brahmins(Telugu,Kannada etc)</li>
+        <li>          Remarriages</li>
+      </ul>
+      <h5>Schemes</h5>
+      <ul>
+        <li>We offer various schemes to suit the different segments. For Iyers and iyengars, we offer Scheme B (Web) and Scheme C (Postal + Web). </li>
+        <li>          For other Brahmins and Remarriages we offer Scheme B only.</li>
+        <li>          In Scheme B, we project the profiles in our website. In Scheme C,in addition to projection, we send monthly charts also by courier every month.</li>
+      </ul>
+      <h5>Tariff</h5>
+<p>Our Tariff rates are kept at very minimal level, in spite,of imposition of service charges.</p>
+<h5>Validity</h5>
+<p>Registrations are valid for a period of 6 Months only  and has to be renewed thereafter We offer concession in Tariff, for renewals if done promptly.</p>
+<h5>Updation</h5>
+      <p>We update the records periodically by contacting the members over Phone/E &ndash;Mail. Once we are informed about settlement of Marriages the profiles are removed and projected in a separate list.</p>
+      <h5>Success Rate</h5>
+      <p>Number of Marriages are getting settled almost daily and thousands have been benefited. The list of Marriage Settled Profiles projected in our website will itself speak volumes for our Success.</p>
+      <h5>Registration</h5>
+<p>Registration can be done in person or by Post/Courier or by on-line. The Registration Forms are made available in our web itself category wise. Payment can be made by Cash/Cheque/DD or through Net Banking(online). As we insist upon the Signature of the profiles in Registration Form the data become more reliable &amp; authentic.</p>
+<h5>Photos</h5>
+<p>We allow 2 Photos for display in our website. Photos are not Compulsory but desirable. For Change of Photos we Charge Rs.100/- per photo. However it can be done freely while renewing the Registration after 6 Months.</p>
+<h5>Modifications</h5>
+<p>Any Modifications in the website can be done by us based upon your instructions over phone or E-mail Free of charge.</p>
+<h5>Charts</h5>
+<p>We publish charts with Basic details every month separately for Iyers &amp; Iyengars relating to the profiles registered in the previous month only. The charts are released in 1st week of every Month and Displayed in our website by 20th .For Remarriage and other Brahmins,Charts are released only periodically .Charts are also made available for sale at normal charges.</p>
+<h5>Swayavarams</h5>
+<p>To enable speedy settlement of marriages we also Organise free Swayavarams(Parents&rsquo; Meet) Periodically. We have conducted number of such Swayavarams so far exclusively for Iyers,Iyengars,other Brahmins,Remarriages ,NRIs and Differently abled as well .</p>
+<h5>Counselling</h5>
+<p>Besides giving various Tips for selection for proper alliance, we also extend pre/post  free counseling services to ensure Happy &amp; Successful Wedded Life.</p>
+<h5>Timings</h5>
+<p>Our office is open from 10.00 AM to 7.00 PM on all days except Tuesday which is a weekly Holiday. A few Staff members are made available on Tuesdays also.</p>
+<h5>Personal Touch</h5>
+<p>We give lot of importance for personal Touch and we are always accessible for any Phone call. We also Keep in Contact with our clients constantly by Sharing Founder&rsquo;s Thoughts through E-Messages.</p>
+<h5>Astrologer</h5>
+<p>Astrologer services and Horoscope casting Facility are available .Astrologer is present on all days except Monday &amp; Tuesday from 11.00 AM to 4.00 PM.</p>
+<h5>About the Founder</h5>
+      <p><!--img border="0" src="images/panchapakesan-founder.jpg" width="121" height="119" style="padding:2px; border:1px solid #F00; margin:10px; float:left;"/!-->Sri.N.Panchapakesan is the Founder of the Organisation .He was earlier employed in I.O.B for Over 30 years in different Capacities and took up Voluntary Retirement for dedicating himself totally to the social cause. He is ably assisted by his daughter Mrs.Niranjana and a Team of dedicated Staff Members.</p>
+       
+      <p>&nbsp;</p>
+      <div style="border: solid 1px #f9c913;background-color: #fffbea; padding:15px;"> <a name="v"></a>
+        <h5>Our Vision</h5>
+        <!--p> <strong>To be the Top Most popular Matrimonial Website for Brahmins always!</strong></p!-->
+         <p> <strong>Our Vision is to be the Top Most Popular Matrimonial Website for Brahmins always and we are Committed for Excellence in extending our Sources.</strong></p>
+
+        <a name="m"></a>
+        <h5>Our Motto</h5>
+        <p><strong> Love All, Serve All!  Help Ever, Hurt Never!!<br />
+          </strong></p>
+        <a name="s"></a>
+        <h5>Our Slogan</h5>
+        <p><strong> Think of Marriage Alliance For Brahmins!<br />
+          Think of Chennai Sai Sankara Matrimonials!</strong> </p>
+        <a name="c"></a>
+        <h5>Our Commitment</h5>
+        <ul>
+          <li>We are committed for the social cause of uniting Brides &amp; Grooms!</li>
+          <li> We are committed for ensuring high Reliability of Data!</li>
+          <li> We are committed for enlarging our Data Bank further &amp; further!</li>
+          <li> We are committed for periodical updating and validation!</li>
+          <li> We are committed for Enlightening on Proper Selection of Alliance!</li>
+          <li> We are committed for service orientation with Nominal charges!</li>
+          <li> We are committed for professional approach with Modernization!</li>
+          <li> We are committed for Personal Touch with Easy Accessibility!</li>
+          <li> We are committed for Prompt redressal of Grievances &amp; Customer Delight!</li>
+          <li> We are committed for extending Pre/Post Counseling for Happy Wedded Life!</li>
+          <li> We are committed for keeping our Team of staff always in High Spirits!</li>
+        </ul>
+        <p><h5>Above All</h5></p>
+        <p> We are Committed for Excellence in our services always!<br />
+        </p>
+      </div>
+      <table width:"990px" border="0" cellspacing="2" cellpadding="0">
+        <tr>
+          <td width="270" ></td>
+          <td width="697" valign="top"><div align="center"></div></td>
+        </tr>
+      </table>
+    </div>
+    
+    <div class="clr"> </div>
+  </div>
+ 
+</div>
+</body>
+</html>
+<script type="text/javascript">
+
+marqueeInit({
+
+uniqueid: 'mycrawler2',
+
+style: {
+
+'padding': '0px',
+
+'width': '970px',
+
+'height': '157px'
+
+},
+
+inc: 5, //speed - pixel increment for each iteration of this marquee's movement
+
+mouse: 'cursor driven', //mouseover behavior ('pause' 'cursor driven' or false)
+
+moveatleast: 1,
+
+neutral: 100,
+
+savedirection: true
+
+});
+
+</script>
